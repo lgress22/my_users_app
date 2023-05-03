ad027443c4223684de015182b03689dd3d0d59fe
liam gress <gress_l@qwasar.io>
2023-05-03 03:57:01 +0000

    my_user_model.rb

--- my_user_model.rb
+++ my_user_model.rb
@@ -1,78 +1,60 @@
 require 'sqlite3'
 
 class User
-    attr_accessor :firstname, :lastname, :age, :email, :password
-
-    def initialize(attributes = {})
-        @firstname = attributes[:firstname]
-        @lastname = attributes[:lastname]
-        @age = attributes[:age]
-        @email = attributes[:email]
-        @password = attributes[:password]
-      end
+    def initialize
+        @db = SQLite3::Database.new("sql.db")
+        execute_sql_file("db.sql")
+        create_table_if_not_exists
+    end
     
-    
-    def self.create
-    db = SQLite3::Database.new('db.sql')
-    db.execute <<-SQL
-        CREATE TABLE IF NOT EXISTS user_info(
-            id INTEGER PRIMARY KEY,
-            firstname TEXT,
-            lastname TEXT,
-            age INTEGER,
-            email TEXT,
-            password TEXT
-        );
-        SQL
-        db.close
+    def execute_sql_file(filename)
+        sql = File.read(filename)
+        statements = sql.split(/;\n/)
+        statements.each {|stmt| @db.execute(stmt)}
     end
 
-    def save
-        db = SQLite3::Database.new('db.sql')
-        db.execute("INSERT INTO user_info(firstname, lastname, age, email, password) VALUES (?,?,?,?,?)",
-            [firstname, lastname, age, email, password])
-        db.close
+    def create(firstname, lastname, age, email, password)
+        puts "Creating user with firstname: #{firstname}, lastname: #{lastname}, age: #{age}, email: #{email}, password: #{password}"
+        
+        @db.execute(
+            "INSERT INTO users(firstname, lastname, age, email, password) VALUES (?,?,?,?,?)",
+            [firstname, lastname, age, email, password]
+        ) 
     end
 
-    def self.all
-        db = SQLite3::Database.new('db.sql')
-        rows = db.execute("SELECT * FROM user_info")
-        db.close
-        rows
+    def find(user_id)
+        result = @db.execute("SELECT * FROM users WHERE user_id = ?", user_id).first
+        result ? result_to_user_object(result) : nil
     end
-  end
+
+    def all
+        @db.execute("SELECT * FROM users").map {|result| result_to_user_object(result)}
+    end
+
+    def update(user_id, firstname, lastname, age, email, password)
+        @db.execute("UPDATE users SET firstname = ?, lastname = ?, age = ?, email = ?, password = ?, WHERE user_id = ?", [firstname, lastename, age, email, password, user_id])
+    end
+
+    def destroy(user_id)
+        @db.execute("DELETE FROM users WHERE user_id = ?", user_id)
+    end
+
+    private
+
+    def create_table_if_not_exists
+        @db.execute("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY AUTOINCREMENT, firstname TEXT, 
+            lastname TEXT, age INTEGER, email TEXT, password TEXT)")
+    end
+
+    def result_to_user_object(result)
+        {id: result[0], firstname: result[1], lastname: result[2], age: result[3], email: result[4], password: result[5]}
+    end
+end
+
   
   
-  User.create
-  
-  @user = User.new(
-    firstname: "John",
-    lastname: "Smith",
-    age: 30,
-    email: "johnsmith@gmail.com",
-    password: "password1234"
-  )
+user = User.new
 
-#lastest_user = User.all.last
-#puts lastest_user.join("\t")
-=begin
-user = User.new("John", "Doe", 34, "johnsmith@hotmail.com", "password456")
-user.create
+user.create("John", "Doe", 30, "johndoe@gmail.com", "password1234")
 
-
-
-
-puts user.firstname
-puts user.lastname
-puts user.age
-puts user.email
-puts user.password
-
-def initialize(firstname, lastname, age, email, password)
-      @firstname = firstname
-      @lastname = lastname
-      @age = age
-      @email = email
-      @password = password
-    end
-=end
+puts user.all
